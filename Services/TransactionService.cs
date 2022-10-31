8838c6d6686c31e4b8a715e1acd6add42bd6ae8a
Sergio0799 <sergiomontoya0723@gmail.com>
2022-10-31 17:47:52 -0500

    Implemented POST request to Transfer money between two accounts

---
+++ Services/TransactionService.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess;
+
+namespace Services;
+public class TransactionService
+{
+    private IDBRepo _repo;
+    public TransactionService() {
+        _repo = new DBRepo();           // FOR dependency injection, COULD switch out with initialized factory??
+    }
+
+    public bool? TransferMoney(int sendingAccount, int receivingAccount, decimal amount) {
+        return _repo.NewTransaction(sendingAccount, receivingAccount, amount);
+    }
+}
