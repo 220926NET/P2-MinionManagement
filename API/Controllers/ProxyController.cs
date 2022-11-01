4951f6c336a4f3fe0245707c63ab7f9da32c3298
ASOUDRY <soudryalex@gmail.com>
2022-10-31 21:33:28 -0400

    Partial Registration Functionality

---
+++ API/Controllers/ProxyController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Services;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProxyController : ControllerBase
+    {
+        ServiceLayer service = new ServiceLayer();
+        [HttpGet("LoginMinion/{username}/{password}")]
+        public async Task<ActionResult<ProxyUserProfile>> LoginUser(string username, string password) {
+               ProxyUserProfile ret = await service.LoginUser(username, password);
+            return Created("Made it", null);
+        }
+         [HttpPost("RegisterMinion")]
+        public async Task<ActionResult<ProxyUserProfile>> RegisterUser(ProxyUserProfile user) {
+              ProxyUserProfile ret = await service.RegisterANewUser(user);
+            return Created("Made it", ret);
+        }
+    }
+}
