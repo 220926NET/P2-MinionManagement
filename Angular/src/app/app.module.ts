6e8845a2870f2095684187df48b17149ace73218
HaizhenHuang <hhuang56@ucmerced.edu>
2022-11-09 23:30:58 -0800

    angular material for login page

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -7,6 +7,18 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule} from '@angular/common/http'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Angular version Responsive web design
+import { FlexLayoutModule } from '@angular/flex-layout';
+// Angualr material Form 
+import {MatFormFieldModule} from '@angular/material/form-field';
+// Angular material Input
+import {MatInputModule} from '@angular/material/input';
+// Angular material Button
+import {MatButtonModule} from '@angular/material/button';
+// Angualr material Card
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +31,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     HttpClientModule,
     NgbModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
